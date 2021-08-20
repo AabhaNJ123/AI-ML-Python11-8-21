eaae88656cb0065befb17574a59ea3e924017979
unknown <AabhaJahagirdar.com>
2021-08-20 14:24:49 +0530

    first

---
+++ WorkPlace/Area.py
@@ -0,0 +1,4 @@
+side=[5,4,7,8,9,3,8,2,6,4]
+for i in side:
+    a=i*i
+    print(f"Area of square of side {i} is : ",a)
---
+++ WorkPlace/ifElse.py
@@ -0,0 +1,27 @@
+a=10
+b=10
+#if else
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+a=20
+b=20
+#if elif else
+if a>b:
+    print("a is greater")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("b is greater")
+
+#nested if
+num=15
+if num>=0:
+    if num==0:
+        print("num is zero")
+    else:
+        print("Num is positive")
+else:
+    print("Num is negative")
